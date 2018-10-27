137950202b790406b5b10e76379277482559ddaa
Bronson Quick <bronson@bronsonquick.com.au>
2018-10-28 08:45:50 +1000

    Update to Extensions 2.0

--- modules/debugging/manifests/init.pp
+++ modules/debugging/manifests/init.pp
@@ -1,45 +1,47 @@
 # A Chassis extension to install and activate common WordPress plugins used for debugging during development
 class debugging (
-  $path = '/vagrant/extensions/debugging'
+	$config,
+	$path = '/vagrant/extensions/debugging',
+	$php_version  = $config[php]
 ) {
-  if ! ( File['/vagrant/content'] ) {
-    file { '/vagrant/content':
-      ensure => 'directory',
-    }
-  }
-  if ! ( File['/vagrant/content/plugins'] ) {
-    file { '/vagrant/content/plugins':
-      ensure => 'directory',
-    }
-  }
-  wp::plugin { 'debug-bar':
-    ensure   => enabled,
-    location => '/vagrant/wp',
-    require  => Class['wp'],
-  }
-  wp::plugin { 'debug-bar-extender':
-    ensure   => enabled,
-    location => '/vagrant/wp',
-    require  => Class['wp'],
-  }
-  wp::plugin { 'debug-bar-cron':
-    ensure   => enabled,
-    location => '/vagrant/wp',
-    require  => Class['wp'],
-  }
-  wp::plugin { 'debug-bar-transients':
-    ensure   => enabled,
-    location => '/vagrant/wp',
-    require  => Class['wp'],
-  }
-  wp::plugin { 'log-deprecated-notices':
-    ensure   => enabled,
-    location => '/vagrant/wp',
-    require  => Class['wp'],
-  }
-  wp::plugin { 'rewrite-rules-inspector':
-    ensure   => enabled,
-    location => '/vagrant/wp',
-    require  => Class['wp'],
-  }
+	if !( File['/vagrant/content'] ) {
+		file { '/vagrant/content':
+		  ensure => 'directory',
+		}
+	}
+	if !( File['/vagrant/content/plugins'] ) {
+		file { '/vagrant/content/plugins':
+		  ensure => 'directory',
+		}
+	}
+	wp::plugin { 'debug-bar':
+		ensure   => enabled,
+		location => '/vagrant/wp',
+		require  => [ Package["php${php_version}-fpm", "php${php_version}-cli"], Class['wp'] ],
+	}
+	wp::plugin { 'debug-bar-extender':
+		ensure   => enabled,
+		location => '/vagrant/wp',
+		require  => [ Package["php${php_version}-fpm", "php${php_version}-cli"], Class['wp'] ],
+	}
+	wp::plugin { 'debug-bar-cron':
+		ensure   => enabled,
+		location => '/vagrant/wp',
+		require  => [ Package["php${php_version}-fpm", "php${php_version}-cli"], Class['wp'] ],
+	}
+	wp::plugin { 'debug-bar-transients':
+		ensure   => enabled,
+		location => '/vagrant/wp',
+		require  => [ Package["php${php_version}-fpm", "php${php_version}-cli"], Class['wp'] ],
+	}
+	wp::plugin { 'log-deprecated-notices':
+		ensure   => enabled,
+		location => '/vagrant/wp',
+		require  => [ Package["php${php_version}-fpm", "php${php_version}-cli"], Class['wp'] ],
+	}
+	wp::plugin { 'rewrite-rules-inspector':
+		ensure   => enabled,
+		location => '/vagrant/wp',
+		require  => [ Package["php${php_version}-fpm", "php${php_version}-cli"], Class['wp'] ],
+	}
 }
